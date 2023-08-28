79cd86ecd12feac3ae163a9bd8c3ef2bc401b299
Jessir Florez <florezhamburgerjessir@gmail.com>
2023-08-28 16:53:17 -0500

    Envío de datos con visualización en pagina web

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const socketIo = require('socket.io');
 const { Client } = require('pg');
 const dgram = require('dgram');
 
-// Crear la instancia de la aplicación Express y del servidor HTTP
 const app = express();
 const server = http.createServer(app);
 
-// Configurar la conexión a la base de datos PostgreSQL
 const dbConfig = {
   user: 'Jessir',
   host: 'localhost',
@@ -27,18 +25,14 @@ dbClient.connect()
     console.error('Error al conectar a la base de datos:', err);
   });
 
-// Configurar el puerto en el que escuchará el servidor
 const PORT = 3000;
 
-// Iniciar el servidor en el puerto especificado
 server.listen(PORT, () => {
   console.log(`Servidor en funcionamiento en el puerto ${PORT}`);
 });
 
-// Configurar Socket.IO para trabajar con el servidor
 const io = socketIo(server);
 
-// Configurar el servidor UDP
 const udpServer = dgram.createSocket('udp4');
 
 const UDP_PORT = 8099;
@@ -51,26 +45,42 @@ udpServer.on('listening', () => {
 udpServer.on('message', (message, remote) => {
   console.log(`Mensaje recibido de ${remote.address}:${remote.port}: ${message}`);
 
-  const insertQuery = 'INSERT INTO ubicaciones (content) VALUES ($1) RETURNING *';
-  const values = [message.toString()];
-  
+  const entrada = message.toString();
+  let latitude = null;
+  let longitude = null;
+  let time = null;
 
-  dbClient.query(insertQuery, values)
-    .then(result => {
-      const insertedData = result.rows[0];
-      console.log('Datos guardados en la base de datos:', insertedData);
+  entrada.split(', ').forEach(item => {
+    if (item.startsWith('Latitude:')) {
+      latitude = parseFloat(item.split(': ')[1]);
+    } else if (item.startsWith('Longitude:')) {
+      longitude = parseFloat(item.split(': ')[1]);
+    } else if (item.startsWith('Time:')) {
+      time =  parseFloat(item.split(': ')[1]);
+    }
+  });
 
-      // Emitir los datos a través de Socket.IO para actualizar la página web
-      io.emit('nuevos-datos', message.toString());
-    })
-    .catch(err => {
-      console.error('Error al guardar datos en la base de datos:', err);
-    });
+  if (latitude !== null && longitude !== null && time !== null) {
+    const insertQuery = 'INSERT INTO ubicaciones (latitud, longitud, tiempo) VALUES ($1, $2, $3) RETURNING *';
+    const values = [latitude, longitude, time];
+
+    dbClient.query(insertQuery, values)
+      .then(result => {
+        const insertedData = result.rows[0];
+        console.log('Datos guardados en la base de datos:', insertedData);
+
+        io.emit('nuevos-datos', message.toString());
+      })
+      .catch(err => {
+        console.error('Error al guardar datos en la base de datos:', err);
+      });
+  } else {
+    console.log('No se pudieron procesar todos los datos.');
+  }
 });
 
 udpServer.bind(UDP_PORT);
 
-// Configurar la ruta para servir la página HTML
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
