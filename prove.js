194a0d4d820e0d2c13bf22cc55d2bc7d312f6913
Jessir Florez <florezhamburgerjessir@gmail.com>
2023-08-29 15:07:33 -0500

    Versión con Timestamp en prove

--- prove.js
+++ prove.js
@@ -1,7 +1,8 @@
 const { Client } = require('pg');
+const moment = require('moment');
 
 // Datos de entrada
-const entrada = "Latitud: 10.913799, Longitud: -74.8216787, Tiempo: 2023-08-20 13:39:23";
+const entrada = "Latitud: 10.913799, Longitud: -74.8216787, Tiempo: 1693275846840";
 let latitud = null;
 let longitud = null;
 let tiempo = null;
@@ -13,9 +14,12 @@ entrada.split(', ').forEach(item => {
     } else if (item.startsWith('Longitud:')) {
         longitud = parseFloat(item.split(': ')[1]);
     } else if (item.startsWith('Tiempo:')) {
-        tiempo = new Date(item.split(': ')[1]);
+        tiempo = parseFloat(item.split(': ')[1]);
     }
 });
+
+// Convertir tiempo a formato deseado
+const formattedTiempo = moment(tiempo).format('YYYY-MM-DD HH:mm:ss');
 
 // Configuración de la conexión a la base de datos
 const client = new Client({
@@ -31,8 +35,8 @@ client.connect()
     .then(() => {
         // Insertar los valores en la base de datos
         if (latitud !== null && longitud !== null && tiempo !== null) {
-            const query = 'INSERT INTO ubicaciones (latitud, longitud, tiempo) VALUES ($1, $2, $3)';
-            const values = [latitud, longitud, tiempo];
+            const query = 'INSERT INTO gps (latitud, longitud, tiempo) VALUES ($1, $2, $3)';
+            const values = [latitud, longitud, formattedTiempo]; // Usar tiempo formateado
             
             return client.query(query, values);
         } else {
